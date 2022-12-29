c56a32e50ed488b116ca91481393e02511a896d4
woxiaoyuan <34067265+woxiaoyuan@users.noreply.github.com>
2022-12-29 16:12:57 -0500

    Initial Commit

---
+++ Allison-Vapor-iOS-App/Allison_Vapor_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Allison_Vapor_iOS_AppApp.swift
+//  Allison-Vapor-iOS-App
+//
+//  Created by 耶啵的小头盔 on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Allison_Vapor_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
